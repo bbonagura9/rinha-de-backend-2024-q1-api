eacf8e3fed2e585a7076b8f62a061a2cb863d593
Bruno Bonagura <bbonagura9@gmail.com>
2024-02-09 22:26:52 -0300

    fix: identation

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"os"
+  "os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+  "github.com/gin-gonic/gin"
+  "github.com/joho/godotenv"
 
-	"github.com/bbonagura9/rinha-de-backend-2024-q1-api/internal/db"
-	"github.com/bbonagura9/rinha-de-backend-2024-q1-api/internal/models"
-	"github.com/bbonagura9/rinha-de-backend-2024-q1-api/internal/routes"
+  "github.com/bbonagura9/rinha-de-backend-2024-q1-api/internal/db"
+  "github.com/bbonagura9/rinha-de-backend-2024-q1-api/internal/models"
+  "github.com/bbonagura9/rinha-de-backend-2024-q1-api/internal/routes"
 )
 
 func main() {
-	if _, err := os.Stat(".env"); err == nil {
-		err := godotenv.Load()
-		if err != nil {
-			panic("Error loading .env")
-		}
-	}
+  if _, err := os.Stat(".env"); err == nil {
+    err := godotenv.Load()
+    if err != nil {
+      panic("Error loading .env")
+    }
+  }
 
   db := db.ConnectDB()
   db.AutoMigrate(&models.Transacao{})
-	db.AutoMigrate(&models.Cliente{})
-	models.InitClientes(db)
+  db.AutoMigrate(&models.Cliente{})
+  models.InitClientes(db)
 
   r := gin.Default()
 
--- internal/models/models.go
+++ internal/models/models.go
@@ -17,36 +17,36 @@ func (Transacao) TableName() string {
 }
 
 type Cliente struct {
-	gorm.Model
-	ID     uint   `gorm:"primaryKey"`
+  gorm.Model
+  ID     uint   `gorm:"primaryKey"`
   Limite uint64 `gorm:"name:limite"`
   Saldo  int64  `gorm:"name:saldo"`
 }
 
 type TransacoesPostBody struct {
-	Valor     uint64
-	Tipo      string
-	Descricao string
+  Valor     uint64
+  Tipo      string
+  Descricao string
 }
 
 func InitClientes(db *gorm.DB) {
-	limites := [6]uint64{
-		0,
-		100000,
-		80000,
-		1000000,
-		10000000,
-		500000,
-	}
-	for idx := uint(1); idx <= 5; idx++ {
-		var cliente Cliente
-		if result := db.First(&cliente, idx); result.RowsAffected == 0 {
-			cliente = Cliente{
-				ID: idx,
-				Limite: limites[idx],
-				Saldo: 0,
-			}
-			db.Create(&cliente)
-		}
-	}
+  limites := [6]uint64{
+    0,
+    100000,
+    80000,
+    1000000,
+    10000000,
+    500000,
+  }
+  for idx := uint(1); idx <= 5; idx++ {
+    var cliente Cliente
+    if result := db.First(&cliente, idx); result.RowsAffected == 0 {
+      cliente = Cliente{
+        ID: idx,
+        Limite: limites[idx],
+        Saldo: 0,
+      }
+      db.Create(&cliente)
+    }
+  }
 }
